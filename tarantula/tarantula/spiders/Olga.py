2179a57be8a2065b3f89b35e73306f9c346f3150
Hugo A. M. Torres <mercutio22@posthocergopropterhoc.net>
2011-10-31 22:23:09 -0200

    Configurei GoogleBot 2.1 como google agent para evitar ser banido.

--- tarantula/tarantula/spiders/Olga.py
+++ tarantula/tarantula/spiders/Olga.py
@@ -9,12 +9,11 @@ class OlgaSpider(CrawlSpider):
     name = 'Olga'
     DOWNLOAD_DELAY = 60 #para tentar evitar ser banido
     ROBOTSTXT_OBEY = True
-    CONCURRENT_REQUESTS = 1
+    #CONCURRENT_REQUESTS = 1
     USER_AGENT = "Googlebot/2.1 ( http://www.google.com/bot.html )"
-#    FEED_URI = 'MedicWebSites.csv'
-#    FEED_FORMAT = 'csv'
+    FEED_URI = '/home/mercutio22/gitcode/medial-saude-web-scrapper/tarantula/MedicWebSites.csv'
+    FEED_FORMAT = 'csv'
     #allowed_domains = ['guiareunimedicos.med.br']
-    #handle_httpstatus_list = [503]
     start_urls = [
         'http://medial-saude.guiareunimedicos.med.br/index.pl?act=searc\
 h&_id_=172&_ev_=Submit&_formSearchSubmit=%3Adefault%3A&type=0&country=0\
@@ -22,10 +21,7 @@ h&_id_=172&_ev_=Submit&_formSearchSubmit=%3Adefault%3A&type=0&country=0\
        'http://www.guiareunimedicos.med.br/index.pl?act=search&_id_=17\
 #2&_ev_=Submit&_formSearchSubmit=%3Adefault%3A&type=0&country=0&q=cancer\
 #ologia#results/' ]
-
     rules = (
-        #Rule(SgmlLinkExtractor(allow=r"V=", restrict_xpaths='//div[co\
-#ntains(@class, "cx-step-full-index")]'), callback='parse_item', follow=True),
         Rule(SgmlLinkExtractor(allow=r"V=", restrict_xpaths='//a[text()=">"]'),
         callback='parse_item', follow=True),
     )
@@ -36,7 +32,7 @@ h&_id_=172&_ev_=Submit&_formSearchSubmit=%3Adefault%3A&type=0&country=0\
         mdata = hxs.select('//div[contains(@class, "mdata")]')
         links = mdata.select('./a/@href').extract()
         names = mdata.select('./a/text()').extract()
-        
+
         items = []
         for index in range(len(names)):
             i = OlgaItem()
